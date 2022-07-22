0623c4b2d6d4562fd6fc55e749c0bba8f1063bbb
Manish Agarwal <59132663+share2learnwidmanish@users.noreply.github.com>
2022-07-22 19:46:15 +0530

    .tf file examples for remote backend
    .tf file examples for remote backend

---
+++ Learning/Remote_backend/final_main.tf
@@ -0,0 +1,48 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+  }
+  cloud {
+    organization = "manishagarwal"
+    workspaces {
+      name = "gettingstarted"
+		}
+	}
+
+}
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "instance_type" {
+	type = string
+	
+}
+
+locals {
+	project_name = "myproject"
+}
+
+resource "aws_instance" "web" {
+  ami           = "ami-0cff7528ff583bf9a"
+  instance_type = var.instance_type
+
+  tags = {
+    Name = "HelloWorld-${local.project_name}"
+  }
+}
+
+module "vpc_example_simple-vpc" {
+  source  = "terraform-aws-modules/vpc/aws//examples/simple-vpc"
+  version = "3.14.2"
+}
+
+output "public_ip" {
+	value = aws_instance.web.public_ip
+
+}
